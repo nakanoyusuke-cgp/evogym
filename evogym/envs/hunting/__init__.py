5eabc577e6d200eb9094aa7b0fb7d227d9ed5a1f
nakano_yusuke_cgp <46030579+nakanoyusuke-cgp@users.noreply.github.com>
2023-02-23 21:47:13 +0900

    モジュール参照のミスを修正

--- evogym/envs/hunting/__init__.py
+++ evogym/envs/hunting/__init__.py
@@ -1,2 +1,2 @@
-from hunt_creeper import HuntCreeper
-from hunt_hopper import HuntHopper
+from evogym.envs.hunting.hunt_creeper import HuntCreeper
+from evogym.envs.hunting.hunt_hopper import HuntHopper
