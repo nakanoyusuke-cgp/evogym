f7678ddd3e45b0525b05253e37cf414e903d7278
nakanoyusuke-cgp <yusukenakano16@gmail.com>
2023-03-07 09:57:19 +0900

    prepared files for execution

--- examples/run_ga.py
+++ examples/run_ga.py
@@ -12,10 +12,11 @@ if __name__ == "__main__":
     run_ga(
         pop_size = 3,
         structure_shape = (5,5),
-        experiment_name = "test_hunting_ga",
+        experiment_name = "test_flyer",
         max_evaluations = 6,
         train_iters = 50,
         num_cores = 3,
+        voxels_pd=np.array([3/8, 1/8, 1/8, 1/8, 1/8, 0, 1/8, 0], np.float64),
         voxels_limits=np.array([-1, -1, -1, -1, -1, 0, 3, 0], np.int64),
         structure_requirement=lambda robot: np.any(robot==VOXEL_TYPES['PRED']),
     )
