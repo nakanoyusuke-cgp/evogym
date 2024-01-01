65e72eafafb6d9a7b56b7979a92bf8dc505bb497
nakanoyusuke-cgp <yusukenakano16@gmail.com>
2024-01-01 19:59:30 +0900

    実験準備

---
+++ my/expr/baselines/huntCreeperBaseline.py
@@ -0,0 +1,51 @@
+import os, sys
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.join(curr_dir, '../../../examples')
+sys.path.insert(0, root_dir)
+
+import random
+import numpy as np
+
+from evogym.utils import VOXEL_TYPES
+from ga.run import run_ga
+
+
+
+if __name__ == "__main__":
+    seed = 0
+    random.seed(seed)
+    np.random.seed(seed)
+    
+    run_ga(
+        pop_size = 25,
+        structure_shape = (5,5),
+        experiment_name = "huntCreeperBaseline",
+        max_evaluations = 500,
+        train_iters = 1000,
+        num_cores = 3,
+        voxels_pd=np.array([
+            3/8,  # EMPTY
+            1/8,  # RIGID
+            1/8,  # SOFT
+            1/8,  # H_ACT
+            1/8,  # V_ACT
+            0,  # FIXED
+            1/8,  # PRED
+            0,  # PREY
+            0,  # VIS
+            ], np.float64),
+        voxels_limits=np.array([
+            -1,  # EMPTY
+            -1,  # RIGID
+            -1,  # SOFT
+            -1,  # H_ACT
+            -1,  # V_ACT
+            0,  # FIXED
+            3,  # PRED
+            0,  # PREY
+            0,  # VIS
+            ], np.int64),
+        structure_requirement=lambda robot: np.any(robot==VOXEL_TYPES['PRED']),
+    )
+
---
+++ my/expr/baselines/huntCreeperBaselineVis.bat
@@ -0,0 +1,1 @@
+python huntCreeperBaselineVis.py --env-name "HuntCreeperBaselineVis-v0" --algo ppo --use-gae --lr 2.5e-4 --clip-param 0.1 --value-loss-coef 0.5 --num-processes 4 --num-steps 128 --num-mini-batch 4 --log-interval 100 --use-linear-lr-decay --entropy-coef 0.01 --eval-interval 50
