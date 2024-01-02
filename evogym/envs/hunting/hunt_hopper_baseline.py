ce9497f5d5ee5ff694fecdf18596980c19657edf
nakanoyusuke-cgp <yusukenakano16@gmail.com>
2024-01-02 15:51:25 +0900

    hopper baseline更新, creeperに準じたパラメータの変更と報酬の計算関数オーバーライド

--- evogym/envs/hunting/hunt_hopper_baseline.py
+++ evogym/envs/hunting/hunt_hopper_baseline.py
@@ -16,7 +16,7 @@ class HuntHopperBaseline(HuntHopper):
 
     def change_params(self):
         # common
-        self.REWARD_RANGE = 0.7
+        self.REWARD_RANGE = 0.5
         self.PROGRESSIVE_REWARD = 0.05
         self.ROBOT_POS = [1, 1]
         self.PREY_POS = [15, 1]
@@ -32,3 +32,14 @@ class HuntHopperBaseline(HuntHopper):
         self.LANDING_CONTROL_STEPS = 5.0
         self.GROUND_THRESHOLD = 0.0075
         
+    # 1.5**2 + 0.5**2 = 2.5
+    def get_reward(self, prey_pred_diffs, sqr_dist_prev):
+        reward = 0
+        sqr_dist = np.min(np.sum((prey_pred_diffs * prey_pred_diffs), axis=0))
+        if sqr_dist < (self.REWARD_RANGE ** 2):
+            # reward += 0.01 / sqr_dist
+            reward += 0.025 / sqr_dist
+        if sqr_dist < sqr_dist_prev:
+            reward += self.PROGRESSIVE_REWARD
+
+        return np.clip(reward, 0., 1.), sqr_dist
